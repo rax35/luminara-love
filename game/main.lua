1dc58ae398b09f82c1a7d4c2054fa3eaa9c9e1c9
Ved Prajapati <104439483+Spector-Studios@users.noreply.github.com>
2026-03-03 07:53:19 +0530

    Import math extensions
    - formatting

--- game/main.lua
+++ game/main.lua
@@ -1,8 +1,9 @@
+require("math_extensions")
 local Gamestate = require("libs.hump.gamestate")
 local battle = require("scenes.battle")
 
 function love.load()
-	love.graphics.setDefaultFilter("nearest")
-	Gamestate.registerEvents()
-	Gamestate.switch(battle)
+    love.graphics.setDefaultFilter("nearest")
+    Gamestate.registerEvents()
+    Gamestate.switch(battle)
 end
