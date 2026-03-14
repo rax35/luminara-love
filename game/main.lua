7c23a3646770b2e855b1011a8fbef74f7cf9790a
Ved Prajapati <104439483+Spector-Studios@users.noreply.github.com>
2026-03-14 10:02:24 +0530

    Very barebones coroutine scheduler

--- game/main.lua
+++ game/main.lua
@@ -4,7 +4,27 @@ require("input")
 local Gamestate = require("libs.hump.gamestate")
 local battle = require("scenes.battle")
 
+Scheduler = {}
+---@type thread[]
+Scheduler.tasks = {}
+function Scheduler:spawn(t)
+    table.insert(self.tasks, t)
+end
+
+function love.update(_)
+    for i = 1, #Scheduler.tasks do
+        coroutine.resume(Scheduler.tasks[i])
+    end
+end
+
 function love.load()
+    local co = coroutine.create(function()
+        while true do
+            print("coroutine...")
+            coroutine.yield()
+        end
+    end)
+    Scheduler:spawn(co)
     love.graphics.setDefaultFilter("nearest")
     Input.registerCallbacks()
     Gamestate.registerEvents()
